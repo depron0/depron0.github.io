2d92e3e8485afbf44167cbfa9adf179ea6ab8d0c
depron0 <sceneronja@gmail.com>
2022-11-04 14:24:19 +0100

    added data folder, with json file, removed authors from server.js

--- server.js
+++ server.js
@@ -3,24 +3,6 @@ const app = express()
 const PORT = 8000
 
 
-const authors = {
-    'j.r.r. tolkien': {
-        'fullName': 'John Ronald Reuel Tolkien',
-        'birthYear': '3 January 1892',
-        'selectedWorks': ['The Hobbit', 'The Lord of the Rings', 'The Silmarillion'], 
-    },
-    'lewis carroll': {
-        'fullName': 'Charles Lutwidge Dodgson',
-        'birthYear': '27 January 1832',
-        'selectedWorks': ['Alice\'s Adventures in Wonderland', 'Through the Looking-Glass', 'What the Tortoise Said to Achilles']
-    },
-    'unknown': {
-        'fullName': 'unknown',
-        'birthYear': 'unknown',
-        'selectedWorks': 'unkown'
-    }
-}
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
