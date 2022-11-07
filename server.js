5bbf51fde71667e624d7c62d3c4825d570aac3ac
depron0 <sceneronja@gmail.com>
2022-11-07 15:20:03 +0100

    changing api to connect with mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
 
 
-app.get('/', (req, res) => {
+MongoClient.connect('mongodb+srv://depressedKiwifruit:twKxDRwmzpdxxfmg@cluster0.pxdc0ae.mongodb.net/?retryWrites=true&w=majority', {useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+/* app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
@@ -11,9 +22,20 @@ app.get('/api', (req, res) => {
     res.sendFile(__dirname + '/data/authors.json')
 })
 
-app.get('/api:name', (req,res) => {
+app.get('/api/:name', (req, res) => {
     const authorName = req.params.name.toLowerCase()
-})
+    const data = fs.readFileSync(__dirname + '/data/authors.json')
+    const elements = JSON.parse(data)
+
+    if(data.includes(authorName)){
+        
+        console.log(elements)
+        console.log('found')
+    } else {
+        res.json(data['unknown'])
+        console.log('not found')
+    }
+}) */
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is now running on port ${PORT}`)
